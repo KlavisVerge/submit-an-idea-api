b6df9a710da4957416c8ef7dae176192b634d5bf
Nick Phillips <imprecatorr@gmail.com>
2018-07-26 22:08:59 -0500

    Initial commit

---
+++ src/submit-an-idea-api.js
@@ -0,0 +1,56 @@
+require('dotenv/config');
+const AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+AWS.config.update({region: 'us-east-1'});
+const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+exports.handler = (event, context) => {
+    if(event){
+        if(!event.body){
+            event.body = {};
+        }else if(typeof event.body === 'string'){
+            event.body = JSON.parse(event.body);
+        }
+    }
+    const required = ['idea'].filter((property) => !event.body[property]);
+    if(required.length > 0){
+        return Promise.reject({
+            statusCode: 400,
+            message: `Required properties missing: "${required.join('", "')}".`
+        });
+    }
+
+    var params = {
+        TableName: 'submit-an-idea',
+        Item: {
+            'hashkey': new Date() + uuidv4(),
+            'message': event.body.game.length > 1000 ? event.body.game.substring(0, 999) : event.body.game.length
+        }
+    };
+    
+    ddb.putItem(params, function(err, data) {
+        if (err) {
+            return context.succeed({
+                statusCode: 200,
+                body: 'There was an error processing your idea. Please try again later.',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Methods': 'POST',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,XAmz-Security-Token',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            });
+        } else {
+            return context.succeed({
+                statusCode: 200,
+                body: 'Your idea was successfully received!',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Methods': 'POST',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,XAmz-Security-Token',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            });
+        }
+    });
+}
---
+++ terraform.tf
@@ -0,0 +1,134 @@
+terraform {
+  backend "local" {
+    path = "tf_backend/submit-an-idea-api.tfstate"
+  }
+}
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "REST_API_ID" {}
+variable "PARENT_ID" {}
+
+data "aws_iam_role" "role" {
+  name = "apis-for-all-service-account"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+}
+
+resource "aws_dynamodb_table" "submit-an-idea" {
+  name           = "submit-an-idea"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "hashkey"
+
+  attribute {
+    name = "hashkey"
+    type = "S"
+  }
+
+  lifecycle {
+    ignore_changes = ["read_capacity", "write_capacity"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.submit-an-idea.name}"
+  role_arn           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/dynamodb.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_DynamoDBTable"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = "${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.dynamodb_table_read_target.scalable_dimension}"
+  service_namespace  = "${aws_appautoscaling_target.dynamodb_table_read_target.service_namespace}"
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 80
+  }
+}
+
+resource "aws_appautoscaling_target" "dynamodb_table_write_target" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.submit-an-idea.name}"
+  role_arn           = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/dynamodb.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_DynamoDBTable"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "dynamodb_table_write_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = "${aws_appautoscaling_target.dynamodb_table_write_target.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.dynamodb_table_write_target.scalable_dimension}"
+  service_namespace  = "${aws_appautoscaling_target.dynamodb_table_write_target.service_namespace}"
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 80
+  }
+}
+
+resource "aws_api_gateway_resource" "submit-an-idea-api-resource" {
+  rest_api_id = "${var.REST_API_ID}"
+  parent_id   = "${var.PARENT_ID}"
+  path_part   = "submit-an-idea-api"
+}
+
+resource "aws_lambda_function" "submit-an-idea-api-function" {
+  filename      = "submit-an-idea-api.zip"
+  function_name = "submit-an-idea-api"
+
+  role             = "${data.aws_iam_role.role.arn}"
+  handler          = "src/submit-an-idea-api.handler"
+  source_code_hash = "${base64sha256(file("submit-an-idea-api.zip"))}"
+  runtime          = "nodejs6.10"
+  timeout          = 20
+}
+
+resource "aws_lambda_permission" "submit-an-idea-permission" {
+  function_name = "${aws_lambda_function.submit-an-idea-api-function.function_name}"
+  statement_id  = "AllowExecutionFromApiGateway"
+  action        = "lambda:InvokeFunction"
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_method" "submit-an-idea-api-method-post" {
+  rest_api_id   = "${var.REST_API_ID}"
+  resource_id   = "${aws_api_gateway_resource.submit-an-idea-api-resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "submit-an-idea-api-integration" {
+  rest_api_id             = "${var.REST_API_ID}"
+  resource_id             = "${aws_api_gateway_resource.submit-an-idea-api-resource.id}"
+  http_method             = "POST"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.submit-an-idea-api-function.invoke_arn}"
+}
+
+module "CORS_FUNCTION_DETAILS" {
+  source      = "github.com/carrot/terraform-api-gateway-cors-module"
+  resource_id = "${aws_api_gateway_resource.submit-an-idea-api-resource.id}"
+  rest_api_id = "${var.REST_API_ID}"
+}
